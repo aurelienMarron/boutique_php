84cc3198b0d4cfad12a7d281712359bd0a0da68d
Aurélien MARRON <aurelien.marron@EDG>
2021-05-20 10:29:49 +0200

    fin etape 2

---
+++ footer.php
@@ -0,0 +1,86 @@
+<!--DEBUT DE MON FOOTER-->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+            name="viewport"
+            content="width=
+    , initial-scale=1.0"
+    />
+    <title>index</title>
+    <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+            crossorigin="anonymous"
+    />
+
+    <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 text-center" ><a href="#" id="footerfaq">FAQ</a> </div>
+
+            <div class="row">
+                <div class="col-12 text-center" ><a href="Formulaire_2.html" id="footercontact">CONTACT</a> </div>
+            </div>
+        </div>
+
+        <!-- Ligne de 12 colonnes -->
+        <div class="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3" id="reseaux">
+            <!-- 1/3 de la largeur (largeur de 4 colonnes)-->
+            <div class="silent col">
+                <a href="index.html">
+                    <img src="photo/logo silent.jpg" alt="Logo silent"
+                    /></a>
+                SILENT
+            </div>
+
+            <!-- 1/3 de la largeur (largeur de 4 colonnes)-->
+            <div class="col">
+                <div class="reseau">
+                    <a href="https://www.facebook.com/RickAstley">
+                        <img src="photo/facebook_1.png" alt="Logo facebook"
+                        /></a>
+
+                    <a href="https://www.instagram.com/silent_fnf/">
+                        <img src="photo/insta_1.png" alt="Logo instagram"
+                        /></a>
+
+                    <a href="https://twitter.com/Silent_Fnf"
+                    ><img src="photo/twitter_1.png" alt="Logo twitter"
+                        /></a>
+                </div>
+            </div>
+
+            <!-- 1/3 de la largeur (largeur de 4 colonnes)-->
+            <div class="col">
+                <div class="campus">
+                    <a href="https://le-campus-numerique.fr/">
+                        <img
+                            src="photo/logo_campus_header_1.png"
+                            alt="Logo campus numerique"
+                            class="logo-campus"
+                        /></a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="texte">
+                <div class="col-12 text-center text-muted">
+                    Site by Silent Corps <br />
+                    Copyright © 2020, Silent<br />
+                    Terms & Conditions - Privacy - Legal Notice
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--FIN DE MON FOOTER-->
+</body>
+</html>
--- item.php
+++ item.php
@@ -1,12 +1,12 @@
 <?php
+include 'header.php';
+
 $name="pinte de biere";
 $prix="10 euros";
 $url="./biere.png";
 
+echo "$name $prix";
+echo '<img src="'.$url.'" alt="" width="200">';
 
-
- echo "$name $prix" ;
- echo '<img src="'.$url.'">';
+include 'footer.php';
 ?>
-
-
