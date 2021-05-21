0dcb1e721c7621b4c1e5d9bcf7ad3d59e51db92a
aurelienMarron <aurelien.marron@le-campus-numerique.fr>
2021-05-21 16:25:53 +0200

    fin de jour 3

--- multidimensional-catalog.php
+++ multidimensional-catalog.php
@@ -13,14 +13,14 @@ $products = [
         "name"=> "cigarettes",
         "price"=> 10,
         "weight"=> 50,
-        "discount"=> null,
+        "discount"=> 0,
         "picture"=> "https://cdn-news.konbini.com/files/2019/10/paquetclope1.jpg?ratio=16:9&w=640",
     ],
     "tabac_a_rouler"=>[
         "name"=> "tabac à rouler",
         "price"=>6,
         "weight"=> 100,
-        "discount"=> null,
+        "discount"=> 0,
         "picture"=> "https://i.ytimg.com/vi/aW9jzMXCEbo/hqdefault.jpg",
     ],
 ];
@@ -35,29 +35,82 @@ $products = [
 <li>Prix:<?php echo $products["briquet"]["price"]?> euros</li>
 <li>Poids:<?php echo $products["briquet"]["weight"]?> grammes</li>
 <li>Reduction:<?php echo $products["briquet"]["discount"]?> %</li>
-<?php echo '<img src="'.$products["briquet"]["picture"].'" alt="" width="200">';?>
 </ul>
+        <img src='<?php echo $products["briquet"]["picture"] ?>' alt="" width="200"';/>
     </div>
+
     <div class="col">
 <ul>
     <li>Nom:<?php echo $products["cigarettes"]["name"]?></li>
     <li>Prix:<?php echo $products["cigarettes"]["price"]?> euros</li>
     <li>Poids:<?php echo $products["cigarettes"]["weight"]?> grammes</li>
     <li>Reduction:<?php echo $products["cigarettes"]["discount"]?> %</li>
-    <?php echo '<img src="'.$products["cigarettes"]["picture"].'" alt="" width="200">';?>
 </ul>
+        <img src='<?php echo $products["cigarettes"]["picture"] ?>' alt="" width="200"';/>
     </div>
+
     <div class="col">
 <ul>
     <li>Nom:<?php echo $products["tabac_a_rouler"]["name"]?></li>
     <li>Prix:<?php echo $products["tabac_a_rouler"]["price"]?> euros</li>
     <li>Poids:<?php echo $products["tabac_a_rouler"]["weight"]?> grammes</li>
     <li>Reduction:<?php echo $products["tabac_a_rouler"]["discount"]?> %</li>
-    <?php echo '<img src="'.$products["tabac_a_rouler"]["picture"].'" alt="" width="200">';?>
 </ul>
+        <img src='<?php echo $products["tabac_a_rouler"]["picture"] ?>' alt="" width="200"';/>
     </div>
 </div>
 </div>
+
+<?php
+echo "debut foreach" . "<br/>";
+foreach($products as $product=>$liste_info){
+    echo $product . "\n" . "<br/>" . "<br/>";
+    foreach($liste_info as $info){
+        echo $info . "\n" ."<br/>";
+    }
+}
+
+
+echo "debut for" . "<br/>";
+$keys=array_keys($products);
+for($i=0; $i<count($products);$i++){
+    echo $keys[$i]. "\n" . "<br/>" . "<br/>";
+    foreach($products[$keys[$i]] as $key => $value){
+        echo $key .":" . $value."<br/>";
+    }
+}
+
+echo "debut while" . "<br/>";
+$i=0;
+while($i < count($products))
+{
+    $i++;
+    echo $keys[$i]. "\n" . "<br/>" . "<br/>";
+    foreach($products[$keys[$i]] as $key => $value){
+        echo $key .":" . $value."<br/>";
+    }
+}
+
+echo "debut while2" . "<br/>";
+$index =0;
+$index2=0;
+while ($index < count($products)) {
+
+    $keys =array_keys($products);
+    $affichage = $keys[$index];
+    echo $affichage. "\n" . "<br/>"."<br/>";
+    $index++;
+    while ($index2 < count($products[$affichage])) {
+
+        $values = array_values($products[$affichage]);
+        $affichage2 = $values[$index2];
+        echo $affichage2 . "\n" . "<br/>" . "<br/>";
+        $index2++;
+    }
+}
+
+?>
+
 
 </html>
 
--- simple-catalog.php
+++ simple-catalog.php
@@ -8,7 +8,36 @@ print_r($products);
 
 $first_item=reset($products);
 $last_item=end($products);
-echo $first_item ."<br/>". $last_item;
+echo $first_item ."<br/>". $last_item . '<br/>';
+echo "$products[0]\n" . "<br/>";
+
+echo "debut while" . "<br/>";
+$index=0;
+while($index < count($products))
+{
+    $product = $products[$index];
+    $index++;
+
+    echo "$product" . "<br/>". "<br/>";
+}
+
+echo "debut do while" . "<br/>";
+$index=0;
+do{
+    $product = $products[$index];
+    $index++;
+    echo $product . "<br/>". "<br/>";
+}
+while($index < count($products));
+
+echo "debut for" . "<br/>";
+for($i=0; $i<count ($products); $i++)
+{
+    echo "$products[$i]" . "<br/>". "<br/>";}
+
+echo "debut for each" . "<br/>";
+foreach($products as $product){
+    echo $product . "<br/>". "<br/>";
+}
 
 include 'footer.php';
-?>
