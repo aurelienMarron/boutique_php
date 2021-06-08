b3e92ba9f84c3601810638ec6a5b4484fe7d0bee
aurelienMarron <aurelien.marron@le-campus-numerique.fr>
2021-06-08 16:56:59 +0200

    fin 08/06

--- database.php
+++ database.php
@@ -1,37 +1,70 @@
 <?php
-function bddconnect(){
-    return   new PDO('mysql:host=localhost;dbname=boutique_ilot_3;charset=utf8', 'Aurelien', 'Calambrit14');
-}
+
+include 'helpers.php';
+
+
+
+$bdd= new PDO('mysql:host=localhost;dbname=boutique_ilot_3;charset=utf8', 'Aurelien', 'Calambrit14');
+
+function callquery($query){
+    global $bdd;
+    $produit = $bdd->query($query);
+    return $produit->fetchAll(Pdo::FETCH_ASSOC);}
+
 
 
 
 function affiche_nom_produit(){
-    $bdd=bddconnect();
-$nom_produit = $bdd->query('SELECT products.name FROM products');
-return $nom_produit->fetchAll(Pdo::FETCH_ASSOC);}
+    $query='SELECT products.name FROM products';
+return callquery($query);}
 
 
 
 
 function produitenrupture(){
-    $bdd=bddconnect();
-$produit_rupture = $bdd->query('SELECT `products`.`name`, `products`.`quantity` FROM `products` WHERE `products`.`quantity` = 0');
-return $produit_rupture->fetchAll(Pdo::FETCH_ASSOC);}
+    $query='SELECT `products`.`name`, `products`.`quantity` FROM `products` WHERE `products`.`quantity` = 0';
+return callquery($query);}
 
 
 
 function totalcmd(){
-    $bdd=bddconnect();
- $numero_totalcmd = $bdd->query('SELECT orders.number, SUM(products.price*order_product.quantity) as total_commande
+    $query='SELECT orders.number, SUM(products.price*order_product.quantity) as total_commande
 FROM products
 INNER JOIN order_product on products.id = order_product.product_id
 INNER JOIN orders on orders.id = order_product.order_id
 GROUP BY orders.number
-');
-while($donnees=$numero_totalcmd->fetch())
-{
-   echo '<br>' .'le total de la commande' . $donnees['number'] . 'est de ' . $donnees['total_commande'];
-}}
+';
+ return callquery($query);
+}
+
+function nbrcmdparclient(){
+    $query='SELECT customers.first_name, customers.last_name, 
+COUNT(orders.customer_id) as nombre_de_commande
+FROM customers
+LEFT JOIN orders on orders.customer_id=customers.id
+GROUP BY customers.first_name, customers.last_name
+ORDER BY customers.first_name DESC';
+    return callquery($query);
+}
+
+
+
+function creation_produit($name,$description,$price,$weight,$image,$quantity,$available,$categorie_id){
+    global $bdd;
+    $req=$bdd->prepare('INSERT INTO products(name,description,price,weight,image,quantity,available,categorie_id) 
+VALUES ( :name,:description,:price,:weight,:image,:quantity,:available,:categorie_id)');
+    $req->execute(array(
+        'name'=>$name,
+        'description'=>$description,
+        'price'=>$price,
+        'weight'=>$weight,
+        'image'=>$image,
+        'quantity'=>$quantity,
+        'available'=>$available,
+        'categorie_id'=>$categorie_id));
+    echo $name .  ' a bien été ajouté';
+    $req->closeCursor();
+}
 
 
 
